5a2ee2048c7178b37aa52efd070661305e0a1fae
Yerkenulan Alshynbekuly <erke260946@gmail.com>
2026-02-01 20:41:38 +0600

    Yerkebulan Alshynbekuly – added some changes

--- src/edu/aitu/oop3/db/entities/Ebook.java
+++ src/edu/aitu/oop3/db/entities/Ebook.java
@@ -4,6 +4,7 @@ public class Ebook extends Book{
     public Ebook(int id, String title, String author, int year, boolean available,String type) {
         super(id, title, author, year, available,type);
     }
+
     @Override
     public String getType() {
         return "Ebook";
--- src/edu/aitu/oop3/db/entities/PrintedBook.java
+++ src/edu/aitu/oop3/db/entities/PrintedBook.java
@@ -1,9 +1,10 @@
 package edu.aitu.oop3.db.entities;
 
-public class PrintedBook extends Book{
+public class PrintedBook  Book{
     public PrintedBook(int id, String title, String author, int year, boolean available, String type) {
         super(id, title, author, year, available,type);
     }
+
     @Override
     public String getType() {
         return "Printed Book";
